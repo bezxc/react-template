9279d4930d57f7d9a606fb25a3e4b5e03ec8c5e6
bezxc <bezxc@icloud.com>
2024-08-11 03:23:54 +0300

    ! fix mobile sidebar

--- src/features/menu/ui/index.tsx
+++ src/features/menu/ui/index.tsx
@@ -23,7 +23,7 @@ export const Menu: FC<MenuProps> = ({ isOpen }) => {
 
   return (
     <ScrollArea className="[&>div>div[style]]:!block">
-      <nav className="mt-8 h-full w-full">
+      <nav className="mt-6 h-full w-full">
         <ul className="flex flex-col min-h-[calc(100vh-48px-36px-16px-32px)] lg:min-h-[calc(100vh-32px-40px-32px)] items-start space-y-1 px-2">
           {menuList.map(({ groupLabel, menus }, index) => (
             <li key={index} className={cn("w-full", groupLabel ? "pt-5" : "")}>
